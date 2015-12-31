d8d08c6fbc89eb8704327e7787ca675a477c9e2e
Tom Gray <tomg77@yahoo.co.uk>
2015-12-31 15:35:08 +0000

    Delete old files

--- dbConnection.py
+++
@@ -1,4 +0,0 @@
-import sqlite3
-from getConfig import getConfPart
-def connect(dbFile=getConfPart('dbFile')):
-	return sqlite3.connect(dbFile)
--- helpers.py
+++
@@ -1,8 +0,0 @@
-import re
-def getArgsBy(argString,regExp,strip=True):
-	# Use regexp to split arguments into list
-	if strip:
-		argString = argString.strip()
-	args = re.split(regExp,argString)
-
-	return args
