ca5ce0c0a968bbff32e735bc15a1a68c82416271
Tom Gray <tomg77@yahoo.co.uk>
2016-01-03 21:31:20 +0000

    Added getCustomerInfoFromId, to be used in remove booking, still to be added

--- app/helpers/helpers.py
+++ app/helpers/helpers.py
@@ -1,5 +1,6 @@
 import re,sqlite3
 from app.db import dbConnection
+from app.helpers.getConfig import getConfPart
 def getArgsBy(argString,regExp,strip=True):
 	# Use regexp to split arguments into list
 	if strip:
@@ -23,7 +24,7 @@ def getCustomerId(name,address=None):
 			if rowsReturned == 2:
 				return "ERROR:nameNotUnique"
 			elif rowsReturned == 1:
-				# Get actual customer id
+			# Get actual customer id
 				# cursor = dbConnection.connect('bookings.db')
 				cursor = conn.execute(
 					"SELECT customerId FROM  customers WHERE name=?",
@@ -64,3 +65,46 @@ def getCustomerId(name,address=None):
 			return "ERROR"
 		finally:
 			conn.close()
+def getCustomerInfoFromId(customerId,attributesWanted=('name','address')):
+	# Get a customers information, by default name and address
+	# From their id, the attryibutes wanted are passed in through
+	# a list
+	
+	statement = "SELECT "
+	if len(attributesWanted)==1:
+		statement += "? "
+	elif len(attributesWanted)==0:
+		return "ERROR:noAttributesToSelect"
+	else:
+		i=0
+		while i<len(attributesWanted):
+			# check the values provided by the user against
+			# customers -> list by, in config, as '?' can't be used
+			# for column names in sql
+			searchableArgs = getArgsBy(getConfPart('listBy','customers').strip(),',')
+			if attributesWanted[i] in searchableArgs:
+				if i != len(attributesWanted)-1 :
+					statement += attributesWanted[i] + ","
+				else:
+					statement += attributesWanted[i]
+			else:
+				return "ERROR:unknownAttributeWanted"
+			i += 1
+	# No need to check if customer is unique as id is used
+	# And is marked so in the db
+	statement += " FROM customers WHERE customerId=?"
+	conn = dbConnection.connect()
+	print(statement)
+	try:
+		cursor = conn.execute(
+			statement,
+			(customerId,)
+		)
+		customerInfo = cursor.fetchall()
+		if customerInfo == [''] or customerInfo == None:
+			return "Error:noMatchingCustomer"
+		else:
+			return customerInfo
+	except sqlite3.Error as e:
+		print("Error: {}".format(e))
+		return "ERROR"
