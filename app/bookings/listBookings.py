e5913aed61caabb77d4cbf68e05b1642e9dc3ffe
Tom Gray <tomg77@yahoo.co.uk>
2016-02-14 16:00:44 +0000

    Removed old list function

--- app/bookings/listBookings.py
+++ app/bookings/listBookings.py
@@ -71,61 +71,6 @@ def getBookings(argsDict):
 		return cursor.fetchall()
 	except sqlite3.Error:
 		raise
-def list(argsString):
-	conn = dbConnection.connect()
-	args = getArgsBy(argsString,',|=')
-	if args == [''] or not args:
-		try:
-			bookings=conn.execute("SELECT * FROM bookings")
-			print(tabulate(bookings,
-				headers=['Booking id','Customer id','Time','Reason'],
-				tablefmt="fancy_grid"))
-		except sqlite3.Error as e:
-			print("Error: {}".format(e))
-	elif len(args) % 2 != 0:
-		# If args length is an uneven length then there is a argument without a pair
-		# value and will mean the sql statement will get messed up
-		print("Odd number of arguments supplied")
-		print("Each argument must have a value eg. \n")
-		print("lb key=value or")
-		print("lb key=value,key2=value2\n")
-		print("There can be no lone keys or values")
-
-	else:
-		searchableArgs = getArgsBy(getConfPart('listBy','bookings').strip(),',')
-		argsFound = 0
-		# Declare base statement then add to it if more args found
-		statement = "SELECT * FROM bookings WHERE "
-		unSorted=True
-		i=0
-		vals=[]
-		while unSorted:
-			if args[i] in searchableArgs:
-				if argsFound >= 1:
-					statement += "AND " + args[i] + '=? '
-				else:
-					statement += args[i] + '=? '
-				vals.append(args[i+1])
-				args.remove(args[i+1])
-				argsFound += 1
-			else:
-				print("Invalid argument: {}".format(args[i]))
-				outputs.decideWhatToDo()
-			i+=1
-			if i >= len(args):
-				unSorted = False
-			# print(args)
-		try:
-			bookings=conn.execute(
-				statement,
-				vals)
-			print(tabulate(bookings,
-				headers=['Booking id','Customer id','Time','Reason'],
-				tablefmt="fancy_grid"))
-		except sqlite3.Error as e:
-			print("Error: {}".format(e))
-	# Go back to start
-	outputs.decideWhatToDo()
 def listAvailable(argsString):
 	# If args is blank then assume that today was wanted
 	args = getArgsBy(argsString,',|=| ')
