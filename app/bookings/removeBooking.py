d3f53a713cc4bc81d6df3205efee5cc543895af3
Tom Gray <tomg77@yahoo.co.uk>
2016-02-16 23:15:57 +0000

    Refactored remove bookings to fit in more with the general style of the rest of the program, i.e has wizzard and actual remove function

--- app/bookings/removeBooking.py
+++ app/bookings/removeBooking.py
@@ -14,15 +14,18 @@ def removeWizzard(argsString):
 	args = getArgsBy(argsString,',|=')
 	if len(args) % 2 != 0:
 		print("Insufficient arguments provided, odd number")
-	attributes = []
-	values = []
-	for i in range(0,len(args)):
-		if i%2 == 0:
-			attributes.append(args[i])
+	
+	searchableArgs=getArgsBy(getConfPart('removeBy','bookings').strip(),',')
+	argsDict = {}
+	
+	for i in range(0,len(args),2):
+		if args[i] in searchableArgs:
+			argsDict[args[i]] = args[i+1]
 		else:
-			values.append(args[i])
+			print("Invalid argument: {}".format(args[i]))
+			outputs.decideWhatToDo()
 	try:
-		if remove(attributes,values):
+		if remove(argsDict):
 			print("Succesfully removed booking")
 		else:
 			print("No bookings deleted")
@@ -32,75 +35,82 @@ def removeWizzard(argsString):
 		print("Type error: {}".format(e))
 	except LookupError as e:
 		print("Lookup error: {}".format(e))
-	except Exception  as e:
-		print("Error: {}".format(e))
 	outputs.decideWhatToDo()
-def remove(attributes,values,confirm=True):
-	#First check that values is a list and that it has the same length
-	#As attributes
-	if not isinstance(values,list):
-		raise TypeError("Values not list")
-	elif len(attributes) != len(values):
-		raise ValueError("Not same amount of attributes and values")
+def remove(argsDict,limit=1,confirm=True):
+	#Deletes bookings matching parameters given, if the amount of bookings matching the
+	#parameters exceedes the limit then a ValueError will be thrown
+	#Also by defualt a confirm message will come up
+	i = 0
 	whereClause = ""
-	unChecked=True
-	argsFound,i=0,0
-	searchableArgs=getArgsBy(getConfPart('removeBy','bookings').strip(),',')
-	while unChecked:
-		if attributes[i] in searchableArgs:
-			if argsFound >= 1:
-				whereClause += "AND " + attributes[i] + '=? '
-			else:
-				whereClause += attributes[i] + '=?'
-				argsFound += 1
+	valsList = []
+	#First check generate a where clause that can be used across both the select and
+	#remove statements
+	for key,value in argsDict.items():
+		if i >=1:
+			whereClause += "AND " + key + "=? "
 		else:
-			raise ValueError("Argument is not searchable")
-		i+=1
-		if i >= len(attributes):
-			unChecked = False
-
+			whereClause += key + "=? "
+		valsList.append(value)
+		i += 1
+	#Append limit +1 to check if too many results are returned
+	valsList.append(str(limit+1))
 	conn = dbConnection.connect()
-	statement = "SELECT count(*),customerId FROM bookings WHERE " + whereClause + " LIMIT 2"
+	statement = "SELECT customerId FROM bookings WHERE " + whereClause + " LIMIT ?"
+	print(statement)
+	#print(valsList)
 	try:
 		cursor = conn.execute(
 			statement,
-			tuple(values)
+			valsList
 		)
-		countAndCustomerId = cursor.fetchone()
-		if countAndCustomerId[0] == 2:
-			raise LookupError("Multiple matching bookings")
-		elif countAndCustomerId[0] == 0:
-			raise LookupError("No booking found")
+		customerIds = cursor.fetchall()
+		print(customerIds)
+		#outputs.decideWhatToDo()
+		if len(customerIds) > limit:
+			raise LookupError("More bookings found than specified limit")
+		elif len(customerIds) == 0:
+			raise LookupError("No bookings found")
 		else:
-			# By defualt this will happen as confirm defualts to True
-			# However in the future this may not be desired and so can 
-			# be set to remove without customer detail confrimation
 			confirmDelete=None
-			if confirm:
-				try:
-					customerInfo = getCustomerInfoFromId(countAndCustomerId[1])
-				except ValueError as e:
-					print("Value error: {}".format(e))
-					outputs.decideWhatToDo()
-				except LookupError as e:
-					print("Lookup error: {}".format(e))
-					outputs.decideWhatToDo()
-				except Exception as e:
-					print("Unexpected error: {}".format(e))
-					outputs.decideWhatToDo()
+			#Loop through all the bookings returned and as it's a nested tuple list
+			#it's in the form customerIds[i][0]
+			for i in range(0,len(customerIds)):
+				# By defualt this will happen as confirm defualts to True
+				# However in the future this may not be desired and so can 
+				# be set to remove without customer detail confrimation
+				if confirm:
+					try:
+						#Loop through
+						customerInfo = getCustomerInfoFromId(customerIds[i][0])
+					except ValueError as e:
+						print("Value error: {}".format(e))
+						outputs.decideWhatToDo()
+					except LookupError as e:
+						print("Lookup error: {}".format(e))
+						outputs.decideWhatToDo()
+					except Exception as e:
+						print("Unexpected error: {}".format(e))
+						outputs.decideWhatToDo()
+					#TODO - Show the time for the booking as the only time when multiple bookings
+					#matching the same whereClause is when there are multiple bookings from the same customer
+					print("Delete booking for {}, address {}".format(customerInfo[0],customerInfo[1]))
+					confirmDelete = input("(y/n): ")
 
-				print("Delete booking for {}, address {}".format(customerInfo[0],customerInfo[1]))
-				confirmDelete = input("(y/n): ")
-			if confirmDelete.lower() == "y" or (confirm==False and confrimDelete==None):
-				deleteCursor = conn.execute(
-					"DELETE FROM bookings WHERE " + whereClause,
-					tuple(values),
-				)
-				conn.commit()
-			elif confirmDelete.lower() == "n":
-				return False
-			else:
-				return False
+				if confirmDelete.lower() == "y" or (confirm==False and confrimDelete==None):
+					print("whereClause: {}HELLO \nvalsList: {}".format(whereClause,valsList))
+					statement = "DELETE FROM bookings WHERE " + whereClause
+					print(statement)
+					#TODO - Fix limit on sqlite delete due to it having to be compiled differently, argh
+					deleteCursor = conn.execute(
+						statement,
+						valsList[:-1]
+					)
+					conn.commit()
+				elif confirmDelete.lower() == "n":
+					return False
+				else:
+					return False
 		return True
 	except sqlite3.Error as e:
 		raise sqlite3.Error(e)
+
